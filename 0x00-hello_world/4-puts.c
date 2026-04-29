922af9825d96bfe003b964c98c1e074f3511cb0f
Abobell <akanobello695@gmail.com>
2026-04-29 16:47:54 +0100

    C program that prints exactly a line of text using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+ /**
+ * Author: Abdulahi Bello
+ * Program: WinMingle Community C Training
+ * Description:program that prints exactly this line, followed by a new line:
+"Programming is like building a multilingual puzzle 
+ */
+#include <stdio.h>
+int main(void){
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
+}
