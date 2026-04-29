c19aec7c9900d92dc91190f5d68897609702b75d
Abobell <akanobello695@gmail.com>
2026-04-29 16:49:08 +0100

    C program that uses printf to print out line of text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+ /**
+ * Author: Abdulahi Bello
+ * Program: WinMingle Community C Training
+ * Description: A C program that prints exactly:
+with proper grammar, but the outcome is a piece of art,
+
+followed by a new line.
+. 
+ */
+#include <stdio.h>
+int main(void){
+printf("with proper grammar, but the outcome is a piece of art,\nfollowed by a new line\n");
+return 0;
+}
