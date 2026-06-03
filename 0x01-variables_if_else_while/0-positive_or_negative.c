cf0b8056a787a5debac4f2a85d97e3d1b90d1c42
Abobell <akanobello695@gmail.com>
2026-06-03 14:26:10 +0100

    postive and negativve number generators

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+ /**
+ * Author: Abdulahi Aiodun Bello
+ * Program: WinMingle Community C Training
+ * Description: program that assigns a random number to the variable n each time it runs and prints whether the number is positive, negative, or zero.
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+int n;
+srand(time(0));
+n=rand()-RAND_MAX/2;
+if(n<0){
+  printf("%d is negative\n",n);
+}
+else if(n>0){
+  printf("%d is positive\n",n);
+ }
+else{
+  printf("%d is negative",n);
+  }
+return 0;
+}
