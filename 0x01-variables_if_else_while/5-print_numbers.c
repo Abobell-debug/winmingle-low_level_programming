b26c62e7cf7f4a0237e0ed65cf0c1e59994c30b7
Abobell <akanobello695@gmail.com>
2026-06-03 15:31:14 +0100

    All base ten single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Abdulahi Abiodun Bello
+ * Program: WinMingle Community C Training
+ * Description: Print all single-digit numbers of base 10 starting from 0.
+ */
+#include <stdio.h>
+int main(void){
+int digits = '0';
+while(digits<='9'){
+putchar(digits);
+digits++;
+}
+putchar('\n');
+return 0;
+}
+
+
+
+
+
