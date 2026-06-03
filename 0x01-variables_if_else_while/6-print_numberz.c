0afdef72df77b3a2139d24ea4275013402b402bb
Abobell <akanobello695@gmail.com>
2026-06-03 15:32:14 +0100

    All base ten digits without using char variable

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+ /**
+ * Author: Abdulahi Abiodun Bello
+ * Program: WinMingle Community C Training
+ * Description: Print all single-digit numbers of base 10 starting from 0.
+ */
+#include <stdio.h>
+int main (void){
+int Numberz = 48;
+while(Numberz<=57){
+putchar(Numberz);
+Numberz++;
+}
+putchar('\n');
+return 0;
+}
+
+
+
+
+
+
+
