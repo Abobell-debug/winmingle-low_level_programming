7fb86380978044315d53891a7c85be32fad15f42
Abobell <akanobello695@gmail.com>
2026-06-12 15:33:43 +0100

    Function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+/**Name: Abdulahi Abiodun Bello
+Program: Winmingle Software Engineering
+Description: A program that checks for lowercase character.
+**/
+
+#include "main.h"
+int _islower(int c){
+if (c >= 'a' && c <= 'z'){
+return 1;
+}
+else{
+return 0;
+}
+}
+
+
