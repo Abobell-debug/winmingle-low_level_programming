a351ee7a9d14b52662d8d203219f787e9347d04a
Abobell <akanobello695@gmail.com>
2026-06-12 15:35:29 +0100

    Function that prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+/** 
+Name: Abdulahi Abiodun Bello
+Program: Winmingle Software Engineering
+Description: a program that prints the last digit of a number.
+**/
+#include "main.h"
+int print_last_digit(int n){
+int last_digit = n %10;
+if (last_digit < 0){
+last_digit = last_digit * -1;
+}
+_putchar(last_digit + '0');
+return last_digit ;
+}
+
+
+
