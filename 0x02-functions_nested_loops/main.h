f2b8c2cc610c4ca746d35f8d041dffcb8062d574
Abobell <akanobello695@gmail.com>
2026-06-12 15:28:27 +0100

    Add function prototypes and header guards

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+/** This code contains a custom function of _putchar to print out the letters of the function out without using printf or putchar function of the system.**/
+
+#ifndef MAIN_H    /* Guard Start: IF Not Defined ... */
+#define MAIN_H    /* Guard Start: ... then Define it */
+
+/* ------------------------------- */
+/* PROTOTYPES GO IN THIS MIDDLE ZONE */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+/* ------------------------------- */
+
+#endif            /* Guard End: End of the logic */
+
+
