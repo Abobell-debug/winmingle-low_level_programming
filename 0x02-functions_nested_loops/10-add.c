ad0677b0f589f648b315c0c10081fd013f74a6da
Abobell <akanobello695@gmail.com>
2026-06-12 15:38:58 +0100

    Function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+ Name: Abdulahi Abiodun Bello
+ Program: Winmingle Software Engineering
+ Description: A program that adds two integers and returns the result.
+ **/
+
+#include "main.h"
+int add(int a, int b){
+return (a+b);
+} 
+
+
+ 
