be137e55eedafb994c365d85db1c3d376cd93294
Abobell <akanobello695@gmail.com>
2026-06-12 15:35:03 +0100

    Function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/** Name: Abdulahi Abiodun Bello
+Program: Winwingle Software Engineering
+Description: function that computes the absolute value of an integer.
+**/
+#include "main.h"
+int _abs(int r){
+if (r < 0){
+return (-r);
+}
+else {
+return (r);
+}
+}
+
+
+
+
