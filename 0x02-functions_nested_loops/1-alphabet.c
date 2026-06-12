a7cdd38ee59c6bb95bb65632c0897de5b3866016
Abobell <akanobello695@gmail.com>
2026-06-12 15:32:40 +0100

    Function that prints the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+/**Name: Abdulahi Abiodun Bello
+Program description : Winmingle Software Engineering
+Code Description: A program that prints alphabet in lowercase **/
+#include "main.h"
+void print_alphabet(void){
+char lower_alphabet = 'a';
+for (lower_alphabet = 'a'; lower_alphabet <= 'z'; lower_alphabet++){
+_putchar(lower_alphabet);
+}
+_putchar('\n');
+}
+
+
+
+
